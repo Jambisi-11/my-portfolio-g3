5f35eb87051345c5201f32dd843f2dcd96f6b61e
jambisi <jambis.banji@yahoo.com>
2024-12-17 20:16:39 +0100

    What peoeple are saying is updated

--- src/Compenets/Content/Home/WhatIDo/WhatIDo.jsx
+++ src/Compenets/Content/Home/WhatIDo/WhatIDo.jsx
@@ -11,7 +11,7 @@ const WhatIDo = () => {
     <div>
       <div className='whatIdo'>
         <h3>What I Do</h3>
-        <ul className='do-1'>
+        {/* <ul className='do-1'>
           <li><img src={Christ} alt="cross" />Practise Christianity</li>
           <li><img src={Preach} alt="Preach" />Preach The Word of God</li>
           <li><img src={web} alt="Web" />Web Sites and Platforms</li>
@@ -20,7 +20,22 @@ const WhatIDo = () => {
           <li><img src={film} alt="film" />Film Editor</li>
           <li><img src={sound} alt="sound" />Sound Engineer</li>
           <li><img src={fashion} alt="fashion" />Fashion Designer</li>
-        </ul>
+        </ul> */}
+
+        <table className="my-table">
+            <tbody>
+              <tr className='cont-flex'>
+                <td className='cont-flex'><img src={Christ} alt="cross" />Practise <br></br>Christianity</td>
+                <td className='cont-flex'><img src={film} alt="film" /> Film <br></br> Editor</td>
+                <td className='cont-flex'><img src={Preach} alt="preach" /> Preach The <br></br>Word of God</td>
+              </tr>
+              <tr className='cont-flex'>
+                <td className='cont-flex'><img src={sound} alt="sound" /> Sound <br></br>Engineer</td>
+                <td className='cont-flex'><img src={web} alt="web" /> Web Sites &<br></br> Platforms</td>
+                <td className='cont-flex'><img src={fashion} alt="fashion" /> Fashion <br></br>Designer</td>
+              </tr>
+            </tbody>
+        </table>
       </div>  
     </div>
   )
