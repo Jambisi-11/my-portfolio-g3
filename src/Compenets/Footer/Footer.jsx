cfe7bfa0efb479a5d85ace32e4d9881e43cfd720
jambisi <jambis.banji@yahoo.com>
2025-01-13 18:26:55 +0100

    Footer structured

--- src/Compenets/Footer/Footer.jsx
+++ src/Compenets/Footer/Footer.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div className='flex gap-8 justify-between mt-[100px]'>
+      <div>Copyright 2022 - All right reserve</div>
+      <div> 
+        <ul className='flex gap-4'>
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+          <li>Dribbble</li>
+        </ul>
+      </div>
+    </div>
+   </div>
   )
 }
 
